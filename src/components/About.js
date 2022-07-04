1b3572f94795bc85622595dc90e883a144b920f6
Chang Shin <cshin7374@gmail.com>
2022-07-03 23:28:58 -0500

    commit tree 9034455418b85dd54ddb33907ae077b1c125359c

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Links from "./links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {isBio(props)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
 
+function isBio(props) {
+  if ((props.bio) && !(props.bio === "")) {
+    return <p>{props.bio}</p>
+  } else {
+    return null
+  }
+}
 export default About;
